7e6cc86ed6762f9ffa3b12021433f22041d21870
Jason Neumark Mickela <jason@rootid.com>
2023-12-07 17:29:33 -0800

    updates for d10

--- assets/styles/30-organisms/100-off-canvas/_off-canvas.js
+++ assets/styles/30-organisms/100-off-canvas/_off-canvas.js
@@ -5,58 +5,77 @@
       // Toggle aria-hidden on the #off-canvas element and toggle aria-expanded on the button.
       // Also toggle the aria-expanded on the local button that controls the off-canvas.
       // ALSO set the keyboard focus to be on the first focusable element in the off-canvas.
-      $('[aria-controls="off-canvas"]', context).once().on('click', function(e) {
-        var $offcanvas = $('#off-canvas');
-        var $overlay = $('.off-canvas__overlay');
-        var $toggle = $('[aria-controls="off-canvas"]');
+      const elements = once('toggleOffCanvasButtons', '[aria-controls="off-canvas"]', context);
+      elements.forEach((element) => {
 
-        toggleOffCanvas($toggle, $offcanvas, $overlay);
+        element.addEventListener('click', function(e) {
+          var $offcanvas = $('#off-canvas');
+          var $overlay = $('.off-canvas__overlay');
+          var $toggle = $('[aria-controls="off-canvas"]');
+
+          toggleOffCanvas($toggle, $offcanvas, $overlay);
+        });
       });
+
+
+      // $('[aria-controls="off-canvas"]', context).once().on('click', function(e) {
+      //   var $offcanvas = $('#off-canvas');
+      //   var $overlay = $('.off-canvas__overlay');
+      //   var $toggle = $('[aria-controls="off-canvas"]');
+      //
+      //   toggleOffCanvas($toggle, $offcanvas, $overlay);
+      // });
 
       // When .off-canvas__overlay is clicked, close the off-canvas and toggle the button that has
       // aria-controls="off-canvas" to aria-expanded="false"
-      $('.off-canvas__overlay', context).once().on('click', function(e) {
-        var $offcanvas = $('#off-canvas');
-        var $overlay = $('.off-canvas__overlay');
-        var $toggle = $('[aria-controls="off-canvas"]');
 
-        toggleOffCanvas($toggle, $offcanvas, $overlay);
+      const overlay = once('ocOverlayClick', '.off-canvas__overlay', context);
+      overlay.forEach((element) => {
+
+        element.addEventListener('click', function(e) {
+          var $offcanvas = $('#off-canvas');
+          var $overlay = $('.off-canvas__overlay');
+          var $toggle = $('[aria-controls="off-canvas"]');
+
+          toggleOffCanvas($toggle, $offcanvas, $overlay);
+        });
       });
 
       // When aria-hidden=false on #off-canvas, trap the tab key to elements within #off-canvas
       // Also, if the user presses escape, close the off-canvas and toggle the button that has
       // aria-controls="off-canvas" to aria-expanded="false"
-      $(window).once().on('keydown', function(e) {
-
-        // If the off-canvas is not visible, do nothing.
-        if ($('#off-canvas').attr('aria-hidden') === 'true') {
-          return;
-        }
-
-        var $offcanvas = $('#off-canvas');
-        var $overlay = $('.off-canvas__overlay');
-        var $toggle = $('[aria-controls="off-canvas"]');
-
-        var $focusable = $offcanvas.find('a[href], button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])');
-        var $first = $focusable.first();
-        var $last = $focusable.last();
-        var keyCode = e.keyCode || e.which;
-
-        if (keyCode === 9) {
-          if (e.shiftKey) {
-            if (document.activeElement === $first[0]) {
-              e.preventDefault();
-              $last.focus();
-            }
-          } else {
-            if (document.activeElement === $last[0]) {
-              e.preventDefault();
-              $first.focus();
-            }
+      once('ocWindowKeydown', 'body').forEach((element) => {
+        $(window).on('keydown', function(e) {
+          // If the off-canvas is not visible, do nothing.
+          if ($('#off-canvas').attr('aria-hidden') === 'true') {
+            return;
           }
-        } else if (keyCode === 27) {
-          toggleOffCanvas($toggle, $offcanvas, $overlay);
-        }
+
+          var $offcanvas = $('#off-canvas');
+          var $overlay = $('.off-canvas__overlay');
+          var $toggle = $('[aria-controls="off-canvas"]');
+
+          var $focusable = $offcanvas.find('a[href], button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])');
+          var $first = $focusable.first();
+          var $last = $focusable.last();
+          var keyCode = e.keyCode || e.which;
+
+          if (keyCode === 9) {
+            if (e.shiftKey) {
+              if (document.activeElement === $first[0]) {
+                e.preventDefault();
+                $last.focus();
+              }
+            } else {
+              if (document.activeElement === $last[0]) {
+                e.preventDefault();
+                $first.focus();
+              }
+            }
+          } else if (keyCode === 27) {
+            toggleOffCanvas($toggle, $offcanvas, $overlay);
+          }
+        });
       });
 
       function toggleOffCanvas($toggles, $offcanvas, $overlay) {
